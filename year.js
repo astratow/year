fc9dfb7f0539cea6d78a6db3d7627cad28c00fba
astratow <davno_pora@mail.com>
2021-09-03 22:39:43 +0100

    this is the only commit

---
+++ year.js
@@ -0,0 +1,4 @@
+/*This is a script to provide current year to the website*/
+/*It might be useful in copyright section*/
+document.write(new Date().getFullYear());
+//It can be added to the HTML after copyright word.
