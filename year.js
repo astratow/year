e1ec0914e2c58bd6b9b581debd46860caf508cf4
astratow <davno_pora@mail.com>
2021-09-05 05:05:19 +0100

    Added currentYear() function call and wirtten some docs

--- year.js
+++ year.js
@@ -1,4 +1,11 @@
-/*This is a script to provide current year to the website*/
-/*It might be useful in copyright section*/
-document.write(new Date().getFullYear());
+/* currentYear */
+//This is a script to provide current year to the website
+//call the function currentYear() to do it
+function currentYear() {
+  document.write(new Date().getFullYear());
+}
+currentYear();
 //It can be added to the HTML after copyright word.
+//example:
+//<p>Copyright &copy <script src="currentYear.js"></script></p>
+
